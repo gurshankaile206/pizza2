904266754966b7bf2fe1f058cdea7f335288c824
Gurshan Kaile <gurshankaile206@gmail.com>
2021-06-18 12:54:06 -0700

    add intial html

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 function Pizza(toppings, size, price) {
-  this.toppings = toppings;
+  this.topping = toppings;
   this.size = size; 
   this.price = price; 
 }
@@ -13,8 +13,22 @@ const meatLovers = new Pizza(["pepperoni", "sausage", "bacon"], "medium", 10);
 // based off users selections calculate price
 // create Pizza Constructor function to hold all the properties of the pizza
 // create different pizza variables using "new" to create a new instance of the properties in the pizza function
-// use .prototype to calculate price
+// use .prototype to output price
 
 Pizza.prototype.customerTotal = function() {
     return "Thank you for choosing Gurshan's Pizza, " + "Your total will be " + meatLovers.prices + " Dollars";
 }
+
+// if we name the functions toppings
+// includes toppings prooperty
+// size property
+// price property 
+// 
+
+// const toppings = new Pizza(["Pepperoni", "Cheese", "Meat Lovers", "Supreme"]);
+// const sizes = new Pizza(["small", "Medium", "Large"]);
+
+// function price(pizza) {
+//  let total = 0; 
+//  for (let i = 0; i < toppings.length; i++)
+// }
