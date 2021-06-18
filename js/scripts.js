d07098cbffa63f9f0bf14cf03489f4f62e5c5313
Gurshan Kaile <gurshankaile206@gmail.com>
2021-06-18 10:14:42 -0700

    add overall strategy in comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
-function Pizza() {
-  this.toppings
-  this.size
-  this.toppings + this.size = this.price
+function Pizza(toppings, size, price) {
+  this.toppings = toppings;
+  this.size = size; 
+  this.price = price; 
 }
+
+const pepperoni = new Pizza("Pepperoni", "medium", 8);
+const cheese = new Pizza("Cheese", "medium", 7);
+const meatLovers = new Pizza("Meat Lovers", "medium", 10);
+
+
+// create a pizza parlor website with radio buttons to choose toppings & size
+// based off users selections calculate price
+// create Pizza Constructor function to hold all the properties of the pizza
+// create different pizza variables using "new" to create a new instance of the properties in the pizza function
+// 
