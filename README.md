be53575bdea85dd40a2ea286c0692fe8198d424a
Gurshan Kaile <gurshankaile206@gmail.com>
2021-06-26 23:13:23 -0700

    add pizza main constructor function

--- README.md
+++ README.md
@@ -1,31 +1,11 @@
 Describe: Pizza(toppings, size, price);
 Test: It should output value of each property when called upon. 
-code: function Pizza(toppings, size, price) {
-  this.toppings = toppings;
-  this.size = size; 
-  this.price = price; 
+code: function Pizza() {
+    this.size = undefined; 
+    this.toppings = undefined; 
+    this.sizePrice = 0; 
+    this.toppingsPrice = 0;
+    this.totalPrice = 0;  
+    let output = new Pizza ('medium', 'cheese', 12, 3);
 }
-const pepperoni = new Pizza("Pepperoni", "medium", 8);
-const cheese = new Pizza("Cheese", "medium", 7);
-const meatLovers = new Pizza("Meat Lovers", "medium", 10);
-Expect((pepperoni.toppings).toEqual("pepperoni"))
-
-Test: It should output value of each property when called upon. 
-code: function Pizza(toppings, size, price) {
-  this.toppings = toppings;
-  this.size = size; 
-  this.price = price; 
-}
-const pepperoni = new Pizza("Pepperoni", "medium", 8);
-const cheese = new Pizza("Cheese", "medium", 7);
-const meatLovers = new Pizza("Meat Lovers", "medium", 10);
-Expect((meatLovers.toppings).toEqual(["pepperoni", "sausage", "bacon"]))
-
-Describe: customerTotal();
-Test: It should output the concatonated string & total price of choosen pizza; 
-code: Pizza.prototype.customerTotal = function() {
-    return "Thank you for choosing Gurshan's Pizza, " + "Your total will be " + meatLovers.price + " Dollars";
-}
-}
-Expect: (meatLovers.customerTotal().toEqual("Thank you for choosing Gurshan's Pizza, Your total will be 10 Dollars"))
-
+Expect((output.size).toEqual('medium'));
--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,20 @@
-function Pizza(toppings, size, price) {
-  this.toppings = toppings;
-  this.size = size; 
-  this.price = price; 
+function Pizza() {
+    this.size = undefined; 
+    this.toppings = undefined; 
+    this.sizePrice = 0; 
+    this.toppingsPrice = 0;
+    this.totalPrice = 0;  
 }
 
-const pepperoni = new Pizza("Pepperoni", "medium", 8);
-const cheese = new Pizza("Cheese", "medium", 7);
-const meatLovers = new Pizza(["pepperoni", "sausage", "bacon"], "medium", 10);
-
-
-// create a pizza parlor website with radio buttons to choose toppings & size
-// based off users selections calculate price
-// create Pizza Constructor function to hold all the properties of the pizza
-// create different pizza variables using "new" to create a new instance of the properties in the pizza function
-// use .prototype to output price
-
-Pizza.prototype.customerTotal = function() {
-    return "Thank you for choosing Gurshan's Pizza, " + "Your total will be " + this.prices + " Dollars";
+Pizza.prototype.addSize = function(size) {
+    this.size = size;
+    if(this.size === 'small'){
+        this.sizePrice = 10; 
+    } else if (this.size === 'medium'){
+        this.sizePrice = 15;
+    } else  {
+        this.sizePrice === 20;
+    } 
+    this.calculateTotal();
 }
 
-// if we name the functions toppings
-// includes toppings prooperty
-// size property
-// price property 
-// 
-
-// const toppings = new Pizza(["Pepperoni", "Cheese", "Meat Lovers", "Supreme"]);
-// const sizes = new Pizza(["small", "Medium", "Large"]);
-
-// function price(pizza) {
-//  let total = 0; 
-//  for (let i = 0; i < toppings.length; i++) {
-//      if("Pepperoni") {
-//          total += 5
-//      } else if
-//  }
-// }
-$(document).ready(function() {
-    ("from#pizzas").submit(function(event) {
-        event.preventDefault();
-        const selectedPizza1 = $("input#pizzaChoice1").val();
-        const selectedPizza2 = $("input#pizzaChoice2").val();
-        const selectedPizza3 = $("input#pizzaChoice3").val();
-
-        const result = pizza.customerTotal();
-        $("#output").text(result);
-    })
-})
